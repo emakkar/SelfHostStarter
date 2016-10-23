7d1cf4ec47aafd87a50eaf3c11a788f7a9449a83
Emil Makkar <emakkar@infusion.com>
2016-10-23 07:28:23 -0400

    add project files

---
+++ SelfHostStarter/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SelfHostStarter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("this is a test");
+        }
+    }
+}
