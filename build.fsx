f89169ae1cce4973fac66fb5886ca7d0d0529bf2
Emil Makkar <emakkar@infusion.com>
2016-10-23 11:03:44 -0400

    add build files

--- build.fsx
+++ build.fsx
@@ -1,1 +1,11 @@
-ECHO is on.
+// include Fake lib
+#r @"packages/FAKE/tools/FakeLib.dll"
+open Fake
+
+// Default target
+Target "Default" (fun _ ->
+    trace "Hello World from FAKE"
+)
+
+// start build
+RunTargetOrDefault "Default"
