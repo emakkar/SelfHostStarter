eeea32ed47d20906844709d4657be638ef287b01
Emil Makkar <emakkar@infusion.com>
2016-10-23 23:14:29 -0400

    readd project files

---
+++ src/SelfHostStarter.Specs/Class1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Machine.Specifications;
+
+namespace SelfHostStarter.Specs
+{
+    public class when_index_action_is_invoked
+    {
+        It should_return_a_ViewResult_with_default_view_name = () =>
+        {
+        };
+    }
+}
